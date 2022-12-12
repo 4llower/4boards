be604c19189041b2f34b3a7ea4948351e3174db5
4llower <i.4llower@gmail.com>
2022-12-12 03:44:42 +0100

    Fix paths

--- apps/4boards/src/app/App.tsx
+++ apps/4boards/src/app/App.tsx
@@ -1,17 +1,18 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Codenames, Layout, Play } from './containers';
+import { Codenames, Play } from './containers';
 import { Providers } from './contexts';
 import './styles';
 
 export function App() {
   return (
     <Providers>
-      <Routes>
-        <Route index path="/play" element={<Play />}></Route>
-        <Route path="/codenames" element={<Codenames />}></Route>
-        <Route path="*" element={<Navigate to="/play" />}></Route>
-      </Routes>
-      <Codenames />
+      <div className="app">
+        <Routes>
+          <Route path="play" element={<Play />}></Route>
+          <Route path="codenames" element={<Codenames />}></Route>
+          <Route path="*" element={<Navigate to="play" />}></Route>
+        </Routes>
+      </div>
     </Providers>
   );
 }
